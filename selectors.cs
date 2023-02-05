a2e3a989e25da6ae6cf520e4ea5187609e003c47
Manoj Kumar Bhola <IB4154@ilabs.com>
2023-02-05 10:32:02 +0530

    link css note

---
+++ selectors.cs
@@ -0,0 +1,78 @@
+selectors
+
+element selectors
+class selectors
+id selectors
+pseoudo selectors
+
+color
+===========
+background
+text color
+
+color name
+rgb(red, green, blue)
+
+0-255
+
+rgb(0,0,255); blue
+
+rgb(255,0,0) red
+
+rgb(0,255,0)
+
+
+
+rgba(red , green, blue, alpha chanel)
+
+rgb . 0-255
+
+a(alpha chaneel): 0.0-1.0
+hexadecimal(RGB) (RRGGBB)
+
+000000 (000) black
+
+ffffff (fff)white
+
+0000ff blue
+
+ff0000 red
+
+00ff00 green
+
+
+
+
+0 1 2 3 4 5 6 7 8 9 A B C D E F
+
+
+Units (px, %, rem, em)
+
+1rem = 16px
+
+Margin
+
+
+padding
+
+border
+
+box model
+
+bonus:
+
+how to create a circle and triangle using css
+
+05-02-2023
+-------------
+
+link, hover,focus,visited, active
+
+list: liststyletype
+
+display; inline, block, none, inline-block
+
+float: left, right
+
+
+
