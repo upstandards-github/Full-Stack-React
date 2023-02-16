cf882075f83e4ee3eb93943dba5654edb7b068a8
Manoj Kumar Bhola <IB4154@ilabs.com>
2023-02-16 10:40:38 +0530

    javascript class 1 veriable

---
+++ Javascript/Notes.cs
@@ -0,0 +1,33 @@
+Notes
+---------
+There are 3 ways , er can add javascript to a html file
+
+inline: 
+internal: <script> inside head, body,
+end of body
+external: scrip
+
+
+Single threaded prog:
+ Execution Context
+How to declare veriable in javascript
+1. var
+2. let
+3. const
+
+Difference between var , let, const with execution Context Data Type
+In js data type are dynamic
+Number
+String
+Boolean
+Null
+Undefined
+Object: array, object, date
+Typeof veriable
+ string in JavaScript must be surrounded by quotes.
+  JavaScript, there are 3 types of quotes.
+
+Double quotes: "Hello".
+Single quotes: 'Hello'.
+Backticks: `Hello`.
+
---
+++ Javascript/index.js
@@ -0,0 +1,17 @@
+// var let const
+
+var a = 2;
+var a = 'Hello World';
+var a = 10;
+// a=> veriable name
+// 2=> is the vaalue of veriable a 
+console.log(a);
+
+let b = 20;
+let c = 40;
+b = 30;
+console.log(b);
+console.log(c);
+
+const d = 10;
+console.log(d);
